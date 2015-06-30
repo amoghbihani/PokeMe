69475aad752cec5866557b025914064c11378cad
unknown <amogh1001@gmail.com>
2015-06-30 23:52:39 +0530

    adding final score page

--- js/constants.js
+++ js/constants.js
@@ -10,13 +10,13 @@ var DOT_TIMEOUT = 2000;
 
 var COLOR = new Array();
 COLOR[0] = "#239945";
-COLOR[1] = "#6600ff"
+COLOR[1] = "#0066ff";
 COLOR[2] = "#FF2E12";
 COLOR[3] = "#199900";
-COLOR[4] = "#7200AC";
+COLOR[4] = "#6600ff"
 COLOR[5] = "#ff3333";
 COLOR[6] = "#33cc00";
-COLOR[7] = "#4617B4";
+COLOR[7] = "#7200AC";
 COLOR[8] = "#cc3300";
 
 var CURRENT_COLOR = COLOR[1];
