8bf88cc8b727987205b0bfc293621fbf3f5727f2
unknown <amogh1001@gmail.com>
2015-07-02 00:45:51 +0530

    bug fixes

--- js/constants.js
+++ js/constants.js
@@ -9,14 +9,14 @@ var DOT_INTERVAL = 1000;
 var DOT_TIMEOUT = 2000;
 
 var COLOR = new Array();
-COLOR[0] = "#239945";
+COLOR[0] = "#cc3300";
 COLOR[1] = "#0066ff";
-COLOR[2] = "#FF2E12";
-COLOR[3] = "#199900";
-COLOR[4] = "#6600ff"
-COLOR[5] = "#ff3333";
-COLOR[6] = "#33cc00";
+COLOR[2] = "#199900";
+COLOR[3] = "#FF2E12";
+COLOR[4] = "#6600ff";
+COLOR[5] = "#33cc00";
+COLOR[6] = "#ff3333";
 COLOR[7] = "#7200AC";
-COLOR[8] = "#cc3300";
+COLOR[8] = "#239945";
 
 var CURRENT_COLOR = COLOR[1];
