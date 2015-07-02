6f8e5230327cf00ad78c315b6b4721e8f2e589b1
unknown <amogh1001@gmail.com>
2015-07-02 23:28:12 +0530

    Adding modes

--- js/constants.js
+++ js/constants.js
@@ -3,20 +3,19 @@
  * Author: Amogh Bihani
  */
 
-var ROWS = 5;
-var COLUMNS = 5;
 var DOT_INTERVAL = 1000;
 var DOT_TIMEOUT = 2000;
 
 var COLOR = new Array();
-COLOR[0] = "#cc3300";
-COLOR[1] = "#0066ff";
-COLOR[2] = "#199900";
-COLOR[3] = "#FF2E12";
-COLOR[4] = "#6600ff";
-COLOR[5] = "#33cc00";
-COLOR[6] = "#ff3333";
-COLOR[7] = "#7200AC";
-COLOR[8] = "#239945";
+COLOR[0] = "#f44336"
+COLOR[1] = "#6200ea"
+COLOR[2] = "#009688"
+COLOR[3] = "#ff5722"
+COLOR[4] = "#9c27b0"
+COLOR[5] = "#2196f3"
+COLOR[6] = "#4caF50"
+COLOR[7] ="#795548"
+COLOR[8] = "#8bc34a"
+COLOR[9] = "#00bcd4"
 
 var CURRENT_COLOR = COLOR[1];
